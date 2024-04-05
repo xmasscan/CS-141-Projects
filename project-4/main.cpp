8aae8e486f59d23bf6338acd2c6eb2672046374c
xmasscan <michaelk30200389@gmail.com>
2024-04-04 21:40:16 -0500

    Finished the Project :)

--- project-4/main.cpp
+++ project-4/main.cpp
@@ -3,7 +3,6 @@
 #include <fstream> // file reading lib
 #include <string>
 #include <vector>
-#include <set> // for neighborhood :)
 #include "Utils.h" // custom distance function
 
 using namespace std;
@@ -105,7 +104,6 @@ class Neighborhood
  *  Name, Square Footage, 'Size' (is SF 10,000+?), Address, Neighborhood, Latitude, Longitude.
  */
 // Example Line: OLD WORLD MARKET,10001,Large,5129 N BROADWAY,UPTOWN,41.9754754,-87.65969701
-
 
 // Takes current line of data within file, creates new Store object, returns pointer to new object.
 Store* getStores(string currentLine)
@@ -276,17 +274,64 @@ void storesInfo(vector<Store*> storeList)
     cout << "Average size of large grocery stores: " << setprecision(1) << avgLarge << endl;
 }
 
+// Sorts any given Vector of Neighborhood Object Pointers from most stores to least.
+vector<Neighborhood*> sortNeighborhood(vector<Neighborhood*> unsortedVector)
+{
+	bool isSmallest;
+	
+	vector<Neighborhood*> sortedVector;
+	// Lazy Sorting Algo
+	// 1. Simply add first value to vector
+	// 2. If current value is less than current value iterated in vector, continue iteration
+	// 3. Else insert value at current position
+	
+	// Step 1.
+	sortedVector.push_back(unsortedVector.at(0));
+	// Step 2.
+	for(int i = 1; i < unsortedVector.size(); ++i)
+	{
+		isSmallest = true;
+		for(int j = 0; j < sortedVector.size(); ++j)
+		{
+			// We only care if the current value is greater than another, otherwise it will make its way toward the back eventuallyi
+			// Step 2 implictly, Step 3 explicitly
+			if(unsortedVector.at(i)->totalStores == sortedVector.at(j)->totalStores)
+			{
+                            if(unsortedVector.at(i)->name < sortedVector.at(j)->name)
+                                sortedVector.insert((sortedVector.begin() + j), unsortedVector.at(i));
+                            else
+                                sortedVector.insert(sortedVector.begin() + (j+1) , unsortedVector.at(i));
+
+                            isSmallest = false;
+                            j = sortedVector.size();
+			}
+			else if(unsortedVector.at(i)->totalStores > sortedVector.at(j)->totalStores)
+			{
+				// C++ nonsense with how iterators specifically work mean I must specify the beginning of the vector for this function.
+				sortedVector.insert( (sortedVector.begin() + j), unsortedVector.at(i) );
+				isSmallest = false;
+				j = sortedVector.size();
+			}
+		}
+		if(isSmallest)
+			sortedVector.push_back(unsortedVector.at(i));
+	}
+	return sortedVector;
+}
+
 // Given an input of all stores and all neighborhoods, find how many large and small stores are in each. 
 // NOTE: I refer to the local neighborhood variable as area bcs its less to type.
 void neighborhoodSizes(vector<Store*> storeList, vector<Neighborhood*> areaList)
 {
     bool isMatch = false;
     int j;
+    vector<Neighborhood*> neighborhoodList;
 
     for(int i = 0; i < storeList.size(); ++i)
     {
         // For this project, not many neighborhoods are present for any given file, so we can just cycle through every single one until we find a match.
         j = 0;
+	isMatch = false;
         while(!isMatch)
         {
             if(storeList.at(i)->neighborhood == areaList.at(j)->name)
@@ -305,20 +350,144 @@ void neighborhoodSizes(vector<Store*> storeList, vector<Neighborhood*> areaList)
             j++;
         }
     }
+
+    // Sorts areaList so that data is output in order from most stores to least.
+    neighborhoodList = sortNeighborhood(areaList);
+
     // At this point, the for loop has terminated, that is every store has been accounted for :)
     // setw(24) seems to be the magic number for formatting :)
-    // TODO: FIX OUTPUT, ADD SORTING BY MOST STORES
+    // TODO: ADD SORTING BY MOST STORES
     // Loops through every Neighborhood object in the vector
-    for(int i = 0; i < areaList.size(); ++i)
+    for(int i = 0; i < neighborhoodList.size(); ++i)
     {	
-	// empty string effectively resets setw() (thanks Sam!)
-        cout << areaList.at(i)->name << setw(24 - areaList.at(i)->name.size()) << "";
+        cout << neighborhoodList.at(i)->name << setw(24 - (neighborhoodList.at(i)->name.size())) << "";
         // Large Stores output :)
-        //cout << areaList.at(i)->largeCount;
+	for(int j = 0; j < neighborhoodList.at(i)->largeCount; ++j)
+	{
+		cout << "L";
+	}
         // Small Stores output :D
-        //cout << areaList.at(i)->smallCount;
+	for(int j = 0; j < neighborhoodList.at(i)->smallCount; ++j)
+	{
+		cout << "S";
+	}
         cout << endl;
     }
+}
+
+// THIRD MENU OPTION
+// Closest Store
+/*
+ * Really simple function: Simply just cycles through every Store in memory and runs the predefined distance function in Utils.h.
+ * We care about two datapoints specifically:
+ * 	- The closest store out of all the stores
+ * 	- The closest Large Store
+ * 	* NOTE: They may be the same store.
+ * We can save the stores which fit these criteria for printing later.
+ * "  location is not a food desert if it is within half a mile of any store, or within one mile of a large store. It is a food desert if neither of those applies. "
+ * Therefore, it can be said that if totalStoresDistance > 0.5 && largeStoresDistance > 1, then we additionally output the line "This location is a food desert."
+ *
+ * For my reference and anyone who wishes to edit this code, the distance function is defined as such:
+ * 	double distance(double lat1, double lon1, double lat2, double lon2)
+ * Wherein lat1 and lon1 belong to the user input lat and lon, whilst the second pair of lat/lon values belongs to that of the current store.
+ */
+
+// Also atp I am aware I could be passing these vectors by reference, but I don't think it is the end of the world to create a temporary variable vector full of pointers as opposed to if I was using objects :)
+void closestStore(vector<Store*> storeList, double userLat, double userLon)
+{
+	// variables are named like xStoresDistance for brevity and sanity but its the minimum distance. I just thought it would be too much to call them something like minTotalStoresDistance! Large enough as is imo
+	// For these "find the minimum/maximum" type problems I always set the initial values as the first element in the list.
+	Store* totalStoresDistance = storeList.at(0);
+	Store* largeStoresDistance = storeList.at(0);
+	// At this point the ...StoresDistance values are just the first terms, so we can use them as shorthand :)
+        // Premptively rounding our minimums to avoid future issues, see comments below for a little more info
+	double totalMin = /*floor(*/distance(userLat, userLon, totalStoresDistance->lat, totalStoresDistance->lon)/* * 10) / 10*/;
+	double largeMin = /*floor(*/distance(userLat, userLon, largeStoresDistance->lat, largeStoresDistance->lon)/* * 10) / 10*/;	
+        // holds distance between current store in vector and the user's lat/lon values. here for brevity and readability. :D
+        double currentDistance;
+
+        // Minimum Distance Finding Algo
+	for(int i = 1; i < storeList.size(); ++i)
+	{
+            currentDistance = distance(userLat, userLon, storeList.at(i)->lat, storeList.at(i)->lon);
+            // Notoriously, Double/Float comparison requires you sell your mortal soul to achieve somewhat consistently.
+            // Therefore, I'm just going to premptively round the distance to the nearest tenth. 
+            // Utils.h includes <math.h> so we can just use ceil()
+            // currentDistance = floor(10 * currentDistance) / 10;
+            // Checker for all stores
+            if(currentDistance < totalMin)
+            {
+                totalStoresDistance = storeList.at(i);
+                totalMin = currentDistance;
+            }
+            // Checker for large stores
+            if(storeList.at(i)->size == "Large")
+            {
+                if(currentDistance < largeMin)
+                {
+                    largeStoresDistance = storeList.at(i);
+                    largeMin = currentDistance;
+                }
+            }
+	}
+        
+	// Output
+        cout << "The closest store is " << fixed << setprecision(1) << totalMin << " miles away." << endl;
+        cout << "\t" << totalStoresDistance->name << " is at " << totalStoresDistance->address << endl;
+	if(totalStoresDistance == largeStoresDistance)
+	{
+	    cout << "That is a large store." << endl;
+	}
+	else
+	{
+            cout << "The closest large store is " << fixed << setprecision(1) << largeMin << " miles away." << endl;
+            cout << "\t" << largeStoresDistance->name << " is at " << largeStoresDistance->address << endl;
+	}
+	if(totalMin > 0.5 && largeMin > 1.0)
+            cout << "This location is a food desert." << endl;
+}
+
+// FOURTH MENU OPTION
+// Search Function
+void storeSearch(const vector<Store*>& storeList, string toFind)
+{
+    bool foundFlag = false;
+    Store* storeMatch = nullptr;
+    vector<Store*> storeMatches;
+
+    int i = 0;
+    // Capitalizes string to match text file convention
+    while(toFind[i])
+    {
+        toFind[i] = toupper(toFind[i]);
+        i++;
+    }
+
+    // Linear Search
+    for(int i = 0; i < storeList.size(); ++i)
+    {
+        if(storeList.at(i)->name.find(toFind) != string::npos)
+        {
+            foundFlag = true;
+            storeMatch = storeList.at(i);
+            storeMatches.push_back(storeMatch);
+        }
+    }
+
+    if(foundFlag)
+    {
+        for(int i = 0; i < storeMatches.size(); ++i)
+        {
+            cout << "Store: " << storeMatches.at(i)->name << endl;
+            cout << "Address: " << storeMatches.at(i)->address << endl;
+            cout << "Community: " << storeMatches.at(i)->neighborhood << endl;
+            cout << "Size: " << storeMatches.at(i)->size << endl;
+            cout << "Square Feet: " << storeMatches.at(i)->squareFeet << endl;
+	    cout << endl;
+        }
+    }
+    else
+        cout << "No stores found." << endl;
 }
 
 int main() 
@@ -328,9 +497,13 @@ int main()
     vector<Store*> storeList;
     vector<Neighborhood*> neighborhoodList;
     bool inMenu = true;
+    string toFind;
+    double userLat;
+    double userLon;
     
-    cout << "Enter filename: " << endl;
+    cout << "Enter filename: ";
     cin >> filename;
+    cout << endl;
     // Reads file and compiles information into a vector of Store objects.
     // readFile returns a vector of pointers to Store objects. We will use these 
     storeList = readFile(filename);
@@ -355,8 +528,25 @@ int main()
             case 2:
                 // Goes through all Store Objects and create Neighborhood Objects. Put objects into list.
                 neighborhoodList = getNeighborhoods(storeList);
-                // TODO: Goes through all neighborhoods and prints neighborhoods and their sizes.
+                // TODO: Goes through all eighborhoods and prints neighborhoods and their sizes.
 		neighborhoodSizes(storeList,neighborhoodList);	                
+                break;
+            case 3:
+                // user input
+                cout << "Enter latitude: ";
+                cin >> userLat;
+
+                cout << "Enter longitude: ";
+                cin >> userLon;
+
+                // function call
+                closestStore(storeList, userLat, userLon);
+                break;
+            case 4:
+		cout << "Enter store to search for: ";
+                cin >> toFind;
+		cout << endl;
+                storeSearch(storeList, toFind);
                 break;
             case 5:
                 inMenu = false;
